08d1b6c1fdbcf5f72a2a7dc5f3a6f0402321d3fd
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2021-08-20 00:06:17 +0200

    add minimize function

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,5 @@
+var phone = document.getElementById("window");;
+var minimized = true;
 
 function menu_in(x) {
     console.log(x);
@@ -8,3 +10,13 @@ function menu_out(x) {
     x.classList.remove("change");
 }
 
+function minimize(){
+    minimized = !minimized;
+    console.log(minimized + " " + phone);
+    if(minimized){
+        phone.style.top = "0";
+    }
+    else{
+        phone.style.top = "90%";
+    }   
+}
