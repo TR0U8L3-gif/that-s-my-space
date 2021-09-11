3019f0fd1550e2292d160901e65e4f3e1fb3682f
Radosław Sienkiewicz <rsienkiewicz88@gmail.com>
2021-09-11 23:27:18 +0200

    new func

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,11 @@
 var phone = document.getElementById("window");
 var minimized = true;
 var closed = true;
+var close_s = true;
+var open_s = false;
+var click = false;
 var close_w = document.getElementById("close");
+var side = document.getElementById("side");
 
 function menu_in(x) {
     console.log(x);
@@ -21,14 +25,47 @@ function minimize_but(){
     else{
         phone.style.top = "80%";
     }
-    console.log(phone_rect.top + "  " + phone_value);   
+}
+
+function close_side(){
+    if(click == false){
+        click = true;
+        if(open_s == false){
+            close_s = !close_s;
+            if(close_s){
+                
+                open_s = true;
+                side.style.left = "-50%";
+                side.style.visibility = "hidden";
+                side.style.opacity = "0";
+                setTimeout(function(){
+                    side.style.left = "100%";
+                },500);
+                setTimeout(function(){
+                    open_s = false;
+                    click = false;
+                },500);
+                
+            }
+            else{
+                side.style.visibility = "visible";
+                side.style.opacity = "1";
+                side.style.left = "0";
+                setTimeout(function(){
+                    click = false;
+                },500);
+            }
+        }
+    }
+    
 }
 
 function close_but(){
     close_w.style.visibility= "visible";
-
+    close_w.style.opacity= "1";
 }
 
 function no_but(){
     close_w.style.visibility= "hidden";
+    close_w.style.opacity= "0";
 }
